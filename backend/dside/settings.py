f2cda03c48bf1ffbdd79ef087c00dd5fb0178dfd
webwizards_server <admin@webwizards.eu>
2018-07-23 10:18:42 -0400

    sd

--- backend/dside/settings.py
+++ backend/dside/settings.py
@@ -81,11 +81,14 @@ WSGI_APPLICATION = 'dside.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dside',
+        'USER': 'dsider',
+        'PASSWORD': 'R2SMS234t1ME',
+        'HOST': '185.69.152.24',
+        'PORT': '5433',
     }
 }
 
