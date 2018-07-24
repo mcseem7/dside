6b3c6c2cfa37fffad005cbff347dca47b4aa3e6e
ApaDoctor <apadoctor@gmail.com>
2018-07-24 16:32:18 +0200

    added domain

--- backend/dside/settings.py
+++ backend/dside/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/statico/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 MEDIA_URL = '/uploads/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "uploads")
