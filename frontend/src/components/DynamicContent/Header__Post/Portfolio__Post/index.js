41d2aefc956c0679e678e8de2a73da151033a6b6
Yuri <jekonik26@gmail.com>
2018-08-27 16:15:37 +0300

    update__path/post

--- frontend/src/components/DynamicContent/Header__Post/Portfolio__Post/index.js
+++ frontend/src/components/DynamicContent/Header__Post/Portfolio__Post/index.js
@@ -27,8 +27,7 @@ export default class PortolioPost extends Component {
              alt={item.name}/>
         <figcaption>
           <h2>{item.name}</h2>
-          <Link to={`/dside/${this.props.dataDside[0].id}`}>See project</Link>
-          <Link to={`/dside/${key + 1}`}>Explore project</Link>
+          <Link to={`/dside/${this.props.dataDside[0].id}`}>Explore project</Link>
         </figcaption>
       </figure>
           </Link>
