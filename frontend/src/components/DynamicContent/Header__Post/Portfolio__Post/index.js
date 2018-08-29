8a38d33dc83735a3633bf267dcc313404ee3019e
maxmacil <admin@webwizards.eu>
2018-08-29 10:40:50 +0200

    Homepage portfolio item unfix + Thumb

--- frontend/src/components/DynamicContent/Header__Post/Portfolio__Post/index.js
+++ frontend/src/components/DynamicContent/Header__Post/Portfolio__Post/index.js
@@ -21,13 +21,13 @@ export default class PortolioPost extends Component {
 <Fragment> {
   this.props.dataItem.map((item,key) => {
   return  <div className="grid">
-      <Link to={`/portfolio/${this.props.dataDside[0].id + 1}`}>
+      <Link to={`/portfolio/${this.props.dataDside[0].id}`}>
           <figure className="effect-marley">
-        <img src={`http://mydside.com/${item.thumbnail_image}`}
+        <img src={`http://mydside.com/${item.similar_items.thumbnail}`}
              alt={item.name}/>
         <figcaption>
           <h2>{item.name}</h2>
-          <Link to={`/portfolio/${this.props.dataDside[0].id + 1}`}>Explore project</Link>
+          <Link to={`/portfolio/${this.props.dataDside[0].id}`}>Explore project</Link>
         </figcaption>
       </figure>
      </Link>
