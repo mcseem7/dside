fe2677c0c11ddce06df55bff85e288dd188ddf43
ApaDoctor <apadoctor@gmail.com>
2018-09-06 16:22:36 +0200

    added domain

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
 # Register your models here.
+from home.models import LimitedOfferTranslation, LimitedOffer
+
+
+class LOTInline(admin.StackedInline):
+    verbose_name_plural = "limited offer translations"
+    verbose_name = "translation"
+    model = LimitedOfferTranslation
+
+    extra = 1
+
+
+class LOAdmin(admin.ModelAdmin):
+    list_display = ['date']
+    inlines = [LOTInline]
+
+
+admin.site.register(LimitedOffer, LOAdmin)
--- backend/home/urls.py
+++ backend/home/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 
 from blog.views import CreateTopicSuggestion, BlogCategoryList, BlogItemDetails, BlogItemList
-from home.views import AddOrder
+from home.views import AddOrder, GetLimitedOffers
 
 urlpatterns = [
     url(r'^addOrder/$', AddOrder.as_view()),
+    url(r'^getLimitedOffers/$', GetLimitedOffers.as_view()),
 ]
