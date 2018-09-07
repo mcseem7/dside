bffd65162dcd71f93685606c6d4a03272d9716ba
maxmacil <admin@webwizards.eu>
2018-09-07 22:21:11 +0200

    LimitedService Layout

--- frontend/src/components/CompanyPages/Services/serviceComponents/LimitedService/index.js
+++ frontend/src/components/CompanyPages/Services/serviceComponents/LimitedService/index.js
@@ -35,22 +35,22 @@ export default class LimitedService extends Component {
                   <div className="timer__back-order">
 
                     <div className="timer-days__order">
-                      <p id="time">21</p>
+                      <p className="time">21</p>
                       <p className="title__timer">days</p>
                     </div>
 
                     <div className="timer-hours__order">
-                      <p id="time">20</p>
+                      <p className="time">20</p>
                       <p className="title__timer">hours</p>
                     </div>
 
                     <div className="timer-minutes__order">
-                      <p id="time">53</p>
+                      <p className="time">53</p>
                       <p className="title__timer">minutes</p>
                     </div>
 
                     <div className="timer-seconds__order">
-                      <p id="time">11</p>
+                      <p className="time">11</p>
                       <p className="title__timer">seconds</p>
                     </div>
 
