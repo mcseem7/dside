4a83fe96a272667a55296da6dd35d91b4d72bcc8
maxmacil <admin@webwizards.eu>
2019-05-14 02:03:08 +0200

    Tryout

--- frontend/src/components/DynamicContent/Header__Post/Portfolio__Post/index.js
+++ frontend/src/components/DynamicContent/Header__Post/Portfolio__Post/index.js
@@ -23,31 +23,32 @@ import Translate, { reactTranslateChangeLanguage } from "translate-components";
 
 
     render() {
-console.log('onLeave', this.props.dataDside);
     return (
 <Fragment> {
-  this.props.dataDside.map((item,key) => {
+  this.props.dataDside.map((item, key) => {
   var agrePullo = '' + key + '00';
         if (agrePullo <= 300){
   var divStyle = agrePullo;
         } else {
         var divStyle = '' + key/2 + '00';
         }
-
   return  <div className="grid" >
       <figure className="effect-marley">
                 <div>
                  <Link  to={`/${this.state.lang}/portfolio/${item.CURL}`}>
          <ScrollAnimation delay={divStyle} animateIn="slideInUp" ><img src={`${process.env.REACT_APP_DOMAIN}/${item.thumbnail}`}
              alt={item.name}/></ScrollAnimation>
-        <figcaption>
+        <figcaption style={{'--tooltip-color':item.hovercolor}}>
           <h2>{item.name}</h2>
           {
             <span>{item.category.name}</span>
         }
+
         </figcaption>
             </Link>
             </div>
+
+<noindex><a href={item.projectlink} className="montelink" rel="nofollow" target="_blank">{item.projectlink}</a></noindex>
       </figure>
 
     </div>
